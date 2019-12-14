3fc5b5b50338587908f5796a22f361fba68b4b4c
unknown <nikhil.bansal@catalyst.sh>
2019-12-14 23:57:45 +0530

    minor changes

--- src/config/vars.js
+++ src/config/vars.js
@@ -17,7 +17,7 @@ const defaultConfig = {
     accessToken: '976104661680717825-Qvgf1QGub1CIiMQKMklzpZkRbdpu7V3',
     accessTokenSecret: '9HAz9M27P5CXR2h3cYKV21L65R9ifd138sokIl28R7IaO'
   },
-  whitelist: ['http://example1.com', 'http://example2.com'],
+  whitelist: [],
   socketPort: 5001,
   socketUrl: '127.0.0.1'
 };
